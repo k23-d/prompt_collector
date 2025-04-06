882809e0cf85f40842a91bb2bdaa423053f2848b
k23-d <pabkarna@gmail.com>
2025-04-06 01:03:10 -0400

    Update dropbox_upload.py

--- dropbox_upload.py
+++ dropbox_upload.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def upload_to_dropbox(file_path):
+def upload_bytes_to_dropbox(file_like_object, path):
+    file_like_object.seek(0)
     dbx = dropbox.Dropbox(os.getenv("DROPBOX_ACCESS_TOKEN"))
-    dropbox_path = f"/PromptCollector/{file_path}"
-
-    with open(file_path, "rb") as f:
-        dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
+    dbx.files_upload(file_like_object.read(), path, mode=dropbox.files.WriteMode.overwrite)
